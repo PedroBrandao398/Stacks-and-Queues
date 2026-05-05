b3ba384531bd2c48d6df517db9408ff6e6b38eac
Pedro Brandão <pancb@iscte-iul.pt>
2026-05-05 17:44:48 +0100

    Create FixedCapacityStackOfStrings class skeleton

---
+++ Resizing Stack.java
@@ -0,0 +1,24 @@
+public class FixedCapacityStackOfStrings {
+  private String[] s;
+  private int N = 0;
+  
+  public FixedCapacityStackOfStrings() {
+    s = new String[2];
+  }
+
+  public boolean isEmpty() {
+    
+  }
+
+  public int size() {
+    
+  }
+  
+  public void push(String item) {
+    
+  }
+  
+  public String pop(){
+    
+  }
+}
