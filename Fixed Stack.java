8f23f77e073da6fcda5befc61a1b5530e5d57ea4
Pedro Brandão <pancb@iscte-iul.pt>
2026-05-05 17:34:14 +0100

    Update Fixed Stack.java

--- Fixed Stack.java
+++ Fixed Stack.java
@@ -1,1 +1,20 @@
+public class FixedCapacityStackOfStrings {
+  private String[] s;
+  private int N = 0;
+  
+  public FixedCapacityStackOfStrings(int capacity) {
+    
+  }
 
+  public boolean isEmpty() {
+    
+  }
+  
+  public void push(String item) {
+    
+  }
+  
+  public String pop(){
+    
+  }
+}
