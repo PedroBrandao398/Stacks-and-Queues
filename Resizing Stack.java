669a7dc1d069e17eb909c72220379fede3ba2257
Pedro Brandão <pancb@iscte-iul.pt>
2026-05-05 17:53:03 +0100

    Update Resizing Stack.java

--- Resizing Stack.java
+++ Resizing Stack.java
@@ -23,10 +23,18 @@ public class FixedCapacityStackOfStrings {
   }
   
   public void push(String item) {
-    
+    if (N == s.length) {
+      resize(2 * s.length);
+    }
+    s[N++] = item;
   }
   
   public String pop(){
-    
+    String item = s[--N];
+    s[N] = null;
+    if (N > 0 && N == s.length / 4) {
+      resize(s.length / 2);
+    }
+    return item;
   }
 }
