9842a2db7927e62f82ec00a69fe0b2753dd068a1
Pedro Brandão <pancb@iscte-iul.pt>
2026-05-06 15:23:05 +0100

    Implement ResizingArrayQueueOfStrings class

--- Resizing Queue.java
+++ Resizing Queue.java
@@ -1,1 +1,24 @@
-
+public class ResizingArrayQueueOfStrings {
+  private String[] q;
+  private int first, last;
+  
+  public ResizingArrayQueueOfStrings() {
+    
+  }
+  
+  public boolean isEmpty() {
+    
+  }
+  
+  public void enqueue(String item) {
+    
+  }
+  
+  public String dequeue() {
+    
+  }
+  
+  private void resize(int capacity) {
+    
+  }
+}
