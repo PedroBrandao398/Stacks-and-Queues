c6540a95f4be8e466fa39bebd5885fd189a22f0c
Pedro Brandão <pancb@iscte-iul.pt>
2026-05-16 10:10:23 +0100

    Update Fixed Stack.java

--- Fixed Stack.java
+++ Fixed Stack.java
@@ -1,25 +1,38 @@
 public class FixedCapacityStackOfStrings {
   private String[] s;
   private int N = 0;
-  
+
+
+  //Criação de objetos FixedCapacityStackOfStrings
   public FixedCapacityStackOfStrings(int capacity) {
+     //Cria array de tamanho capacity
     s = new String[capacity];
   }
 
+
+  //Verifica se a stack está vazia
   public boolean isEmpty() {
     return N == 0;
   }
 
+
+  //Retorna o número de elementos da stack
   public int size() {
     return N;
   }
-  
+
+
+  //Adiciona elementos no topo da stack
   public void push(String item) {
     s[N++] = item;
   }
-  
+
+
+  //Remove e retorna o elemento no topo
   public String pop(){
+    //Guarda o topo
     String item = s[--N];
+    //Remove o topo
     s[N] = null;
     return item;
   }
