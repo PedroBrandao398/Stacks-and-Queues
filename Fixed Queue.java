5d7265f971d7ef61fddfbea0d7dfb3b2511e94a2
Pedro Brandão <pancb@iscte-iul.pt>
2026-05-16 11:12:21 +0100

    Update Fixed Queue.java

--- Fixed Queue.java
+++ Fixed Queue.java
@@ -1,34 +1,49 @@
 public class FixedArrayQueueOfStrings {
   private String[] q;
   private int first, last;
-  private int N;
 
   public FixedArrayQueueOfStrings(int capacity) {
     q = new String[capacity];
-    first = 0;
-    last = 0;
-    N = 0;
+    first = -1;
+    last = -1;
   }
 
+
+  //Retorna a próxima posição para a ocupação circular
   private int next(int i) {
     return (i + 1) % q.length;
   }
-  
+
+
+  //Verifica se a queue está vazia
   public boolean isEmpty() {
-    return N == 0;
+    return first == -1;
   }
-  
+
+
+  //Devolve o o número de elementos
   public int size() {
-    return N;
+    //Se não tem elementos/se first está primeiro/se last está primeiro
+    if (first == -1) {
+        return 0;
+    }
+    else if (first <= last) {
+        return last - first + 1;
+    } 
+    else {
+        return q.length - first + last + 1;
+    }
   }
 
   public void enqueue(String item) {
-    if (N == q.length) {
+    if (size() == q.length) {
       throw new IllegalStateException("Error: Queue overflow ");
     }
-    q[last] = item;
+    q[next(last)] = item;
     last = next(last);
-    N++;
+    if(first==-1) {
+      first=0;
+    }
   }
   
   public String dequeue() {
